3289fbcbc4b748d6347d2e2395b345e0c121fbe2
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-06-06 16:42:34 -0700

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(string):
+    for char in string:
+        if ord('a') <= ord(char) <= ord('z'):
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+        print(uppercase_char, end='')
+    print()
