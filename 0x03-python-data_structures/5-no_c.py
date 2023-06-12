15ee9713fabf856019880cd125b656eb2b03256b
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-06-12 11:05:24 -0700

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ''
     for let in my_string:
-        if let != 'c' or let != 'C':
+        if let != 'c' and let != 'C':
             new_string += let
-        return (new_string)
+    return (new_string)
