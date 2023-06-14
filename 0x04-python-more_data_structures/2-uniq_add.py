aa7d1ee6251968cb9d488d52854375caefa85a22
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-06-14 15:37:55 -0700

    2. Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+    sum_unique = 0
+
+    for number in my_list:
+        if number not in unique_numbers:
+            unique_numbers.add(number)
+            sum_unique += number
+
+    return sum_unique
