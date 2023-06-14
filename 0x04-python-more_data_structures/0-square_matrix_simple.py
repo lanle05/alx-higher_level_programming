bf1a3e809ec05fc453dc484b6ad75024c35c2acc
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-06-14 14:17:38 -0700

    0. Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    result = [[0 for _ in row] for row in matrix]
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            result[i][j] = matrix[i][j] ** 2
+
+    return result
