2e36c06e602729bf9c833f8161bd974143bc7c8a
Omobolanle <omobolanle.shaibu@gmail.com>
2023-09-28 02:18:39 +0100

    colon

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
+// A class Rectangle that defines a rectangle
+
 class Rectangle {
   constructor(w, h) {
-    if (
-      w <= 0 ||
-      h <= 0 ||
-      typeof w !== "number" ||
-      typeof h !== "number" ||
-      !Number.isInteger(w) ||
-      !Number.isInteger(h)
-    ) {
-      this.width = 0;
-      this.height = 0;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
