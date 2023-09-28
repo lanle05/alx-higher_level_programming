e090ec22172e2b192be65f8a9720e581f8a178e4
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-09-27 18:17:07 -0700

    colon

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (
       w <= 0 ||
       h <= 0 ||
-      typeof w !== "number" ||
-      typeof h !== "number" ||
+      typeof w !== 'number' ||
+      typeof h !== 'number' ||
       !Number.isInteger(w) ||
       !Number.isInteger(h)
     ) {
