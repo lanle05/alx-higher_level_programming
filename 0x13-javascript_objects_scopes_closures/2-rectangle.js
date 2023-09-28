6489fecd33f0a60765148cc3f2a02c61725c2688
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-09-27 18:24:25 -0700

    colon

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 // A class Rectangle that defines a rectangle
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
