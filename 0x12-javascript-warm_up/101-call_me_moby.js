52bcb486ffc9d90f8d551fccabc6ae9967831da6
Omobolanle Shaibu <117745472+lanle05@users.noreply.github.com>
2023-11-22 14:12:48 +0100

    Delete 0x12-javascript-warm_up directory Deferral

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// A function that executes x times a function
-
-exports.callMeMoby = function (x, theFunction) {
-  let i = 0;
-  for (i = 0; i < x; i++) theFunction();
-};
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-// A function that increments and calls a function
-
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
-};
