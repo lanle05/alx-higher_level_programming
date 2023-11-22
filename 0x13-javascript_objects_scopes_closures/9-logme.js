4f4177a8e66ce76bd761d97fb3b367c80c2aaa98
Omobolanle Shaibu <117745472+lanle05@users.noreply.github.com>
2023-11-22 14:13:44 +0100

    Delete 0x13-javascript_objects_scopes_closures directory deferral

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// A function that prints no. of args already printed and new argument value
-
-let i = 0;
-exports.logMe = function (item) {
-  console.log(i + ': ' + item);
-  i++;
-};
